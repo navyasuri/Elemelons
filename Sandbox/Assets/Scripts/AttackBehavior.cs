135e93bb6dd233424b93338ef1c7ed4d5e3569ce
Syed Faizan Haider Zaidi <sfz219@nyu.edu>
2018-03-26 00:02:02 +0400

    Finalized some mechanics

---
+++ Sandbox/Assets/Scripts/AttackBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehavior : MonoBehaviour {
+
+//	public Transform target;
+//	public float speed;
+	Vector3 newPos;
+	GameObject player;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material.color = Color.red;
+		GameObject.Find ("Player");
+		rb = gameObject.GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		float step = speed * Time.deltaTime;
+//		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+//		newPos=transform.position;
+//		newPos.x += 0.1f;
+//		transform.position = newPos;
+		rb.AddForce(transform.position*1f);
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject == GameObject.Find("Player1"))
+			Debug.Log ("Attacked player");
+			Destroy(gameObject);
+	}
+}
