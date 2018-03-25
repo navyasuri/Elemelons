a83f5b23b5cd630d34bdc2967f355f8174e012be
Syed Faizan Haider Zaidi <sfz219@nyu.edu>
2018-03-25 22:46:15 +0400

    Added Unity file

---
+++ Sandbox/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
