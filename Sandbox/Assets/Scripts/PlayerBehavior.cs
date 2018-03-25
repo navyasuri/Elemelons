21ac07943cfb1c39c2f994f22504728bae2ee0ae
Syed Faizan Haider Zaidi <sfz219@nyu.edu>
2018-03-26 00:01:07 +0400

    Added some mechanics

--- Sandbox/Assets/Scripts/PlayerBehavior.cs
+++ Sandbox/Assets/Scripts/PlayerBehavior.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
 
+	public GameObject attack;
+
 	// Use this for initialization
 	void Start () {
+		// Pick a random, saturated and not-too-dark color
 		GetComponent<Renderer> ().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	void OnMouseDown()
+	{
+		GameObject.Instantiate (attack, new Vector3 (transform.position.x+2*attack.GetComponent<Transform>().localScale.x, 
+			1f, transform.position.z), transform.rotation);
+
 	}
 }
