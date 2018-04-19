5d094a0f03598e980620498a516ee9965f0c80d8
Ali <>
2018-04-19 06:26:15 +0400

    Created the environment for the game

---
+++ TheProject/Assets/Scripts/BoulderBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderBehavior : MonoBehaviour {
+
+	private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+//		this.gameObject.GetComponent<MeshRenderer> ().material.color = Color.red;
+		audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag != "Environment") {
+			
+			Destroy (this.gameObject);
+		}
+	}
+}
