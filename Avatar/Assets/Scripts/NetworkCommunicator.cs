fe1f506a9243de7da8c38d3cd21e3d2f18dd7f01
kotavy <tkk269@nyu.edu>
2018-04-25 07:12:58 +0400

    Testing NewtorkManager with velocity for fireballs

--- Avatar/Assets/Scripts/NetworkCommunicator.cs
+++ Avatar/Assets/Scripts/NetworkCommunicator.cs
@@ -9,15 +9,22 @@ public class NetworkCommunicator : Photon.MonoBehaviour
     // To store the position and rotation of our GameObject as received from the network:
     Vector3 targetPosition;
     Quaternion targetRotation;
+	Vector3 targetVelocity;
+	Rigidbody rb;
+
+	void Start() {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
 
     void Update() {
         // Check if we control this GameObject. If not, deal with data that doesn't update smoothly
         // by constantly Lerping between the position of the GameObject as we see it,
         // and the position that we get from stream.RecieveNext of OnPhotonSerializeView.
         if (!photonView.isMine) {
-            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime * 5f);
-            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 5f);
-        }
+			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, Time.deltaTime * 5f);
+			gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, targetRotation, Time.deltaTime * 5f);
+			rb.velocity = Vector3.Lerp (rb.velocity, targetVelocity, Time.deltaTime * 5f);
+		}
     }
 
     // This method allows us to serialize data and send/receive it over the network,
@@ -27,11 +34,13 @@ public class NetworkCommunicator : Photon.MonoBehaviour
 			// If we own this GameObject, send the data to other players (Use: float/ints/vectors/quaternions)
             stream.SendNext(transform.position); // Send the position.
             stream.SendNext(transform.rotation); // Send the rotation.
+			stream.SendNext(rb.velocity); // Send the velocity.
         }
         else
         { // If we don't own this object, update its data according to what we receive:
             targetPosition = (Vector3)stream.ReceiveNext(); // Store the received position in our targetPosition.
             targetRotation = (Quaternion)stream.ReceiveNext(); // Store the received rotation in our targetRotation.
+			targetVelocity = (Vector3)stream.ReceiveNext(); // Store the receivec velocity in our targetVelocity.
         }
     }
 }
