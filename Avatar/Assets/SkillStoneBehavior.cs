ec97db19e25b1548390b361c69eb5c00d0e6e223
mingwho <mh3833@nyu.edu>
2018-05-03 06:39:47 +0400

    Added skill stone to the game scene

---
+++ Avatar/Assets/SkillStoneBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillStoneBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//start particle system
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "player") {
+			//start UI tutorial
+			//when tutorial ends, stop particle system
+		}
+	}
+}
