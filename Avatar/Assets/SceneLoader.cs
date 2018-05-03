360d3a3ce97da8f5c16954dfd4b90deb7bbdc5bf
Nav <navya.suri13@gmail.com>
2018-05-03 09:56:37 +0400

    added scene manager to lobby

---
+++ Avatar/Assets/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour {
+
+	// void Start ()
+	// {
+	// 	Debug.Log("Working");
+	// }
+
+
+	public void goToScene (string sceneName)
+	{
+
+		Debug.Log("Button being clicked");
+		SceneManager.LoadScene(sceneName);
+		// Application.LoadLevel("about-us");
+	}
+
+	public void goToScene (int sceneNumber)
+	{
+
+		Debug.Log("Button being clicked");
+		SceneManager.LoadScene(sceneNumber);
+		// Application.LoadLevel("about-us");
+	}
+
+	public void LoadScene(int level)
+	{ 
+		Application.LoadLevel(level);
+	}
+
+}
