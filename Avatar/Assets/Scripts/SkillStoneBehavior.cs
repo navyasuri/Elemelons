71d2d6405d05fa3dbb315bf4bafb6e9aec8a0d5b
--global <sfz219@nyu.edu>
2018-05-06 05:08:39 +0400

    Game logic updates

--- Avatar/Assets/Scripts/SkillStoneBehavior.cs
+++ Avatar/Assets/Scripts/SkillStoneBehavior.cs
@@ -9,58 +9,29 @@ public class SkillStoneBehavior : Photon.MonoBehaviour {
 	private bool playerReady;
 	public bool levelEnd;
 	public bool playerSees;
-	public float range;
-	private float dist;
-
+	private bool hasBeenCalled;
 
 	// Use this for initialization
 	void Start () {
 		//start particle system
 
-		range = 2f;
 		levelEnd = false;
 		playerSees = false;
 		playerReady = false;
 
 		Debug.Log ("Skill stone says active scene is: " + SceneManager.GetActiveScene ().name);
-//		if(SceneManager.GetActiveScene().name.Equals("Lobby")) {
-//			gameObject.GetComponent<SphereCollider> ().enabled = true;
-//		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		if (SceneManager.GetActiveScene ().name == "Lobby" || levelEnd) {
-//			dist = Vector3.Distance (this.transform.position, GameObject.Find ("[CameraRig]").transform.position);
 			if (playerSees) {
+				levelEnd = false;
 				unlockSkill ();
 			}
 		}
-
 	}
-
-//	void OnTriggerEnter(Collider col){
-//		Debug.Log ("Collision with Skill Stone by: " + col.gameObject.tag);
-//		if (col.gameObject.CompareTag("Player")) {
-//			//start UI tutorial
-//			//when tutorial ends, stop particle system
-//			if (!playerReady && SceneManager.GetActiveScene ().name == "Lobby") {
-//				Debug.Log ("[Skill stone] Player is now ready!");
-//				playerReady = true;
-//				Debug.Log ("[Skill stone] Are you master client? " + PhotonNetwork.isMasterClient);
-//				GameObject.Find("NetworkManager").GetPhotonView().RPC("UpdateReadyCount", PhotonTargets.All);
-//			}
-//			else {
-//				
-//				StartCoroutine ("resetter");
-//			}
-//
-//		}
-//
-//
-//	}
 
 	private void unlockSkill(){
 		Debug.Log ("Collision by player!");
@@ -73,23 +44,20 @@ public class SkillStoneBehavior : Photon.MonoBehaviour {
 		} 
 
 		else {
-
+			GameObject.Find ("GameManager").GetComponent<GameController> ().addSkill ();
 			StartCoroutine ("resetter");
 		}
-
 	}
 
-
 	IEnumerator resetter(){
-		
-		yield return new WaitForSeconds (10f);
+		yield return new WaitForSeconds (5f);
 
-		GameObject.Find ("GameManager").GetComponent<GameController> ().increaseLevel ();
 		gameObject.transform.Find("Spotlight").gameObject.SetActive(false);
 		gameObject.transform.Find ("Flames").GetComponent<ParticleSystem> ().Stop ();
 		playerSees = false;
+		GameObject.Find ("GameManager").GetComponent<GameController> ().increaseLevel ();
 
 	}
-
+		
 
 }
